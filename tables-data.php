92ffe2413199cab0cadacc8d8da3dd224bc370a6
Pat <patsandesu@gmail.com>
2024-08-25 21:27:19 +0800

    /

--- tables-data.php
+++ tables-data.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Tables / Data - NiceAdmin Bootstrap Template</title>
+  <title>Clinc Management System / Data Tables</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   <link href="assets/img/bcp logo.png" rel="icon">
