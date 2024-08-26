9e7262d9885f5496e47d1bc43344f1a71ab82c7f
Pat <patsandesu@gmail.com>
2024-08-26 20:58:53 +0800

    /

--- forms-elements.php
+++ forms-elements.php
@@ -450,7 +450,6 @@
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-
   <script src="assets/js/main.js"></script>
 </body>
 </html>
