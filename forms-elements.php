80deb12d9171d465af3417cd9ecffc9539160841
Pat <patsandesu@gmail.com>
2024-08-26 20:59:05 +0800

    /

--- forms-elements.php
+++ forms-elements.php
@@ -441,7 +441,6 @@
     </section>
   </main><!-- End #main -->
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <!-- Vendor JS Files -->
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.umd.js"></script>
