3c9c6325faf3555dae4c3cec08e84281ba669871
Pat <patsandesu@gmail.com>
2024-08-30 17:26:00 +0800

    /

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - PrecisionCare Hospital</title>
+    <title>Login - CMS</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <style>
