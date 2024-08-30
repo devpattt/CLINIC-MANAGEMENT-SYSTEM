a363da8446be49b11f8594a6e00f4116de2d3d77
Pat <patsandesu@gmail.com>
2024-08-30 17:25:26 +0800

    /

--- assets/js/loginsc.js
+++ assets/js/loginsc.js
@@ -1,10 +1,9 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
     const accountId = document.getElementById('accountId').value;
     const password = document.getElementById('password').value;
 
-    // Basic validation
     if (accountId === '' || password === '') {
         alert('Please fill in both fields');
     } else {
