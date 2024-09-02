cf073aba372ad1236c2fed179b6c644007f2b666
Pat <patsandesu@gmail.com>
2024-09-02 17:38:28 +0800

    /

--- index.php
+++ index.php
@@ -315,7 +315,7 @@
       </li><!-- End Contact Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
+        <a class="nav-link collapsed" href="pages-register.php">
           <i class="bi bi-card-list"></i>
           <span>Register User</span>
         </a>
