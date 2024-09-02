0a0923fdd1e706f74ebbe3402aa6cbed45e72598
Pat <patsandesu@gmail.com>
2024-09-03 03:12:13 +0800

    :bb

--- forms-elements.php
+++ forms-elements.php
@@ -24,7 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $condition = $_POST['condition'];
     $note = $_POST['note'];
 
-    // Prepare the SQL statement
     $stmt = $conn->prepare("INSERT INTO patient_info (fullname, student_number, contact, gender, age, temperature, date, time, `condition`, note) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssisssss", $fullname, $student_number, $contact, $gender, $age, $temperature, $date, $time, $condition, $note);
 
