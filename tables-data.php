59813ba8d5ef6e4eeb0f91fbd33cf1ee14b96e1e
Pat <patsandesu@gmail.com>
2024-09-02 16:46:54 +0800

    modify table colors

--- tables-data.php
+++ tables-data.php
@@ -271,12 +271,12 @@
         <ul id="forms-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
           <li>
             <a href="forms-elements.php" >
-              <i class="bi bi-circle"></i><span>Add Record</span>
+              <i class="bi bi-circle"></i><span>Add Records</span>
             </a>
           </li>
           <li>
             <a href="tables-data.php" class="active">
-              <i class="bi bi-circle"></i><span>Data Tables</span>
+              <i class="bi bi-circle"></i><span>Patient Records</span>
             </a>
           </li>
         </ul>
@@ -363,7 +363,7 @@
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.php">Home</a></li>
           <li class="breadcrumb-item">Health Records</li>
-          <li class="breadcrumb-item active">Data Tables</li>
+          <li class="breadcrumb-item active">Patient Records</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
@@ -1103,21 +1103,6 @@
     </section>
 
   </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
-  </footer><!-- End Footer -->
-
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
