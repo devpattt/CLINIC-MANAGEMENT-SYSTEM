a4f2e10c03f99f8f64fb6472fbb322bafaae2c5b
Pat <patsandesu@gmail.com>
2024-09-05 21:04:40 +0800

    /

--- forms-elements.php
+++ forms-elements.php
@@ -8,7 +8,6 @@ $dbname = "bcpclinic_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
