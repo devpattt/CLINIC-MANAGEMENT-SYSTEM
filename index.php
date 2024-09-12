5c86e92e33f20a0abee3f55b0fdfd23e3e77cf56
Pat <patsandesu@gmail.com>
2024-09-12 20:17:19 +0800

    some changes

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<? 
+<?php
 session_start();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -190,62 +189,61 @@ session_start();
 
           </ul><!-- End Messages Dropdown Items -->
 
-        </li><!-- End Messages Nav -->
-
-        <li class="nav-item dropdown pe-3">
+          <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">Admin</span>
-          </a><!-- End Profile Iamge Icon -->
+            <img src="assets/img/default profile.jpg" alt="Profile" class="rounded-circle">
+            <span id="fname" class="d-none d-md-block dropdown-toggle ps-2">Ayusin mo to later</span>
+         </a>
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-              <h6>Admin</h6>
-              <span>Nurse</span>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-person"></i>
-                <span>My Profile</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+              <li class="dropdown-header">
+                <h6>Kevin Anderson</h6>
+                <span>Web Designer</span>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                <i class="bi bi-gear"></i>
-                <span>Account Settings</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                  <i class="bi bi-person"></i>
+                  <span>My Profile</span>
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                <i class="bi bi-question-circle"></i>
-                <span>Need Help?</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <li>
+                <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                  <i class="bi bi-gear"></i>
+                  <span>Account Settings</span>
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="logout.php">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
-            </li>
+              <li>
+                <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                  <i class="bi bi-question-circle"></i>
+                  <span>Need Help?</span>
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
 
-          </ul><!-- End Profile Dropdown Items -->
+              <li>
+                <a class="dropdown-item d-flex align-items-center" href="logout.php">
+                  <i class="bi bi-box-arrow-right"></i>
+                  <span>Sign Out</span>
+                </a>
+              </li>
+
+            </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
 
       </ul>
@@ -316,14 +314,6 @@ session_start();
           <span>Contact</span>
         </a>
       </li><!-- End Contact Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.php">
-          <i class="bi bi-card-list"></i>
-          <span>Register User</span>
-        </a>
-      </li><!-- End Register Page Nav -->
-
       <li class="nav-item">
         <a class="nav-link collapsed" href="pages-blank.html">
           <i class="bi bi-file-earmark"></i>
@@ -887,7 +877,7 @@ session_start();
     fetchData('year');
 
     // Add event listener to filter dropdown items
-    document.querySelectorAll('.dropdown-item').forEach(item => {
+    document.querySelectorAll('.dropdown-menu .dropdown-menu-end .dropdown-menu-arrow').forEach(item => {
         item.addEventListener('click', function(event) {
             event.preventDefault();
             const filter = this.getAttribute('data-filter');
@@ -895,6 +885,33 @@ session_start();
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    function updateProfileName() {
+        fetch('fetch_uname.php') // URL to your PHP script
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const nameSpan = document.getElementById('fname');
+                if (data.fname) {
+                    nameSpan.textContent = data.fname; // Update the span with the fetched name
+                } else if (data.error) {
+                    console.error('Error from PHP script:', data.error);
+                } else {
+                    console.error('Unexpected response format');
+                }
+            })
+            .catch(error => console.error('Error fetching name:', error));
+    }
+
+    updateProfileName(); // Call the function to update the profile name on page load
+});
+
+
 
 
 
